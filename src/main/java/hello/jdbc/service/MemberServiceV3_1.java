6a1f75aa5facdc415e92424153ff0993872e408f
Judalm Park (dami) <wnekfa1004@naver.com>
2023-04-16 16:44:10 +0900

    트랜잭션 무제 해결 - 트랜잭션 매니저1

---
+++ src/main/java/hello/jdbc/service/MemberServiceV3_1.java
@@ -0,0 +1,64 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV3;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * 트랜잭션 - 트랜잭션매니저
+ */
+@Slf4j
+@RequiredArgsConstructor
+public class MemberServiceV3_1 {
+
+    private final MemberRepositoryV3 memberRepositoryV3;
+    private final PlatformTransactionManager transactionManager;
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+
+        TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+
+        try {
+            bizLogic(fromId, toId, money);
+            transactionManager.commit(status);
+        } catch (Exception e) {
+            transactionManager.rollback(status);
+            throw new IllegalArgumentException(e);
+        }
+
+
+    }
+
+    private void bizLogic(String fromId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepositoryV3.findById(fromId);
+        Member toMember = memberRepositoryV3.findById(toId);
+
+        memberRepositoryV3.update(fromId, fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepositoryV3.update(toId, toMember.getMoney() + money);
+    }
+
+    private void release(Connection con) {
+        if (con != null) {
+            try {
+                con.setAutoCommit(true); // 바꿔주고 풀에 돌려줘야 다른사람이 문제없이 사용함 default가 true기 때문
+                con.close();
+            } catch (Exception e) {
+                log.info("error", e);
+            }
+        }
+    }
+
+    private void validation(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalArgumentException("이체중 예외 발생");
+        }
+    }
+}
